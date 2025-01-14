677037548507076d77e9e1702719408453ba35d7
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-01-14 15:26:42 -0500

    Add output for workspace name in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "workspace_id" {
   value       = tfe_workspace.ws.id
   description = "ID of Workspace."
 }
+
+output "workspace_name" {
+  value       = tfe_workspace.ws.name
+  description = "Name of Workspace."
+}
